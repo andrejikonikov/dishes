e3a73ac373f94a397765ec89a21829048d86fc44
andrejikonikov <andrejikonikov@gmail.com>
2026-01-18 23:36:30 +0200

    simplify dish component

--- src/components/menu/components/dish.tsx
+++ src/components/menu/components/dish.tsx
@@ -8,19 +8,7 @@ type DishProps = {
 }
 
 function Dish({ dish, addOrder }: DishProps) {
-  console.log('Dish rendered ', dish.title)
-  const { title, description, price, type } = dish
-
-  if (type === 'drink') {
-    return (
-      <div className="menu__dish">
-        <h2>{title}</h2>
-        <p>{description}</p>
-        <p>Price: ${price}</p>
-        <Button text="Order" onClick={() => addOrder(dish)} />
-      </div>
-    )
-  }
+  const { title, description, price } = dish
 
   return (
     <div className="menu__dish">
